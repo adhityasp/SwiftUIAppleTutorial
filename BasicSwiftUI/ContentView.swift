0d9a792cd5380414a080b67bc3f74f3fe0812549
Adhitya Surya Pratama <19931734+adhityasp@users.noreply.github.com>
2020-01-05 21:08:27 +0700

    Initial Commit

---
+++ BasicSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BasicSwiftUI
+//
+//  Created by Adhitya Surya Pratama on 05/01/20.
+//  Copyright © 2020 Adhitya Surya Pratama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BasicSwiftUITests/BasicSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  BasicSwiftUITests.swift
+//  BasicSwiftUITests
+//
+//  Created by Adhitya Surya Pratama on 05/01/20.
+//  Copyright © 2020 Adhitya Surya Pratama. All rights reserved.
+//
+
+import XCTest
+@testable import BasicSwiftUI
+
+class BasicSwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
